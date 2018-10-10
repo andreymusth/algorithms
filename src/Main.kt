cfd78c17bbc84beb58614bb846dc41fd35424e04
Andrey Belous <andreyrabo@mail.ru>
2018-10-10 22:22:03 +0300

    added median method

--- src/Main.kt
+++ src/Main.kt
@@ -3,21 +3,27 @@ import array.HighArray
 
 fun main(vararg args: String) {
 
-    val arr = HighArray(20)
+    val maxSize = 10
+    val arr = HighArray(maxSize)
+
     arr.insert(44)
     arr.insert(24)
     arr.insert(11)
-    arr.insert(78)
+//    arr.insert(78)
     arr.insert(19)
     arr.insert(55)
     arr.insert(34)
     arr.insert(66)
     arr.insert(1)
 
+    println()
+
+    arr.display()
+    println()
+    arr.insertionSort()
     arr.display()
     println()
 
-    arr.insertionSort()
-    arr.display()
+    println(arr.median())
 
 }
