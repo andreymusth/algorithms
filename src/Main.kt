b634e39aee8cedc7eb5c9a1b53aadf683089bbfe
Andrey Belous <andreyrabo@mail.ru>
2018-10-13 10:47:33 +0300

    added simple array based stack

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,5 @@
-import array.HighArray
 
 
 fun main(vararg args: String) {
-
-    val maxSize = 10
-    val arr = HighArray(maxSize)
-
-    arr.insert(44)
-    arr.insert(24)
-    arr.insert(11)
-//    arr.insert(78)
-    arr.insert(19)
-    arr.insert(55)
-    arr.insert(34)
-    arr.insert(66)
-    arr.insert(1)
-
-    println()
-
-    arr.display()
-    println()
-    arr.insertionSort()
-    arr.display()
-    println()
-
-    println(arr.median())
 
 }
