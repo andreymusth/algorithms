28fb467ebf952493ff34f1269a8e551d6c4897c4
Andrey Belous <andreyrabo@mail.ru>
2018-10-22 19:27:44 +0300

    cmmt

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,16 @@
-
+import queue.PriorityQ
 
 fun main(vararg args: String) {
 
+    val thePQ = PriorityQ(5)
+
+    thePQ.insert(30)
+    thePQ.insert(50)
+    thePQ.insert(10)
+    thePQ.insert(40)
+    thePQ.insert(20)
+    while (!thePQ.isEmpty()) {
+        val n = thePQ.remove()
+        print("$n ")
+    }
 }
